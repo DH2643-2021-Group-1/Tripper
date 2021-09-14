7acf1c45d758a1d7ba190a8a714d1f4d936aad6f
Adam Jonsson <adamsmajl@gmail.com>
2021-09-14 15:11:46 +0200

    feat: Add base for backend server
    Co-authored-by: Kristina Andersson <anderssonk@users.noreply.github.com>

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+// rest of the code remains same
+const app = express();
+const PORT = 8000;
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+
+app.get('/test', (req, res) => res.send('YES!!!'));
