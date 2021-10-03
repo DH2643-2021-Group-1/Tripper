2569e9b1c960ae6364250d0f38c6d8b81e9ae337
annagustavsson <annalovisaellen@gmail.com>
2021-10-03 20:10:25 +0200

    feat: add inital set blogpost logic

--- server/src/firestore/firestore.ts
+++ server/src/firestore/firestore.ts
@@ -9,11 +9,29 @@ class BlogPost {
     constructor(title: string, text: string) {
         this.title = title;
         this.text = text;
+    }
+}
 
+const setBlogPost = async (req: express.Request, res: express.Response) => {
+    // TODO: should hold reference to user
+    try {
+        // TODO make not hardcoded - > const obj = JSON.parse(req.body) // title: obj.title, text: object.text
+        const blogPostData = {
+            title: "Blog post title",
+            text: "The text in the blogpost"
+            //userRef: "how is this set?"
+        }
+        const doc = await firestore.collection('blogposts').add(blogPostData)
+        console.log('Added document with ID: ', doc.id);
+        res.send("New blogpost written to database")
+    }
+    catch (error) {
+        res.status(400).json({ error: 'an error occurred writing blogpost to database' })
     }
 }
 
 const getBlogPost = async (req: express.Request, res: express.Response) => {
+    // TODO: make not hardcoded, (and fix firestore layout)
     try {
         const blopostArray = []
         const doc = await firestore
@@ -30,7 +48,7 @@ const getBlogPost = async (req: express.Request, res: express.Response) => {
         res.send(blopostArray)
     } catch (error) {
         console.log("error:(")
-        res.status(400).json({ error: 'an error occurred' })
+        res.status(400).json({ error: 'an error occurred getting blogpost' })
     }
 }
 
@@ -40,5 +58,6 @@ const testFunc = (req: express.Request, res: express.Response) => {
 
 module.exports = {
     getBlogPost,
+    setBlogPost,
     testFunc
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-const { getBlogPost, testFunc } = require("./firestore/firestore")
+const { getBlogPost, setBlogPost, testFunc } = require("./firestore/firestore")
 // rest of the code remains same
 const app = express();
 const PORT = 8000;
@@ -8,11 +8,21 @@ app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
 
+// TODO datan ska kunna skickas i headers : getblogPost(blogpostsID), setBlogPost({"blogpostitle", "blogpostext"})
+
 app.get('/test', (req, res) => res.send('YES!!!'));
 
 
 app.get('/blogposts', (req, res) => {
   getBlogPost(req, res)
+})
+
+// app.post('/blogposts', (req, res) => {
+//   setBlogPost(req, res)
+// })
+
+app.get('/blogg', (req, res) => {
+  setBlogPost(req, res)
 })
 
 app.get("/skoj", (req, res) => {
