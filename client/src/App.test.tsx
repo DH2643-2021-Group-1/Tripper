28d17a33176cd2e4a8758ec1d924f93308eabdc6
Adam Jonsson <adamsmajl@gmail.com>
2021-10-07 10:30:53 +0200

    fix: Fix App.tsx tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Tripper/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getAllByText(/Tripper/i);
+  expect(linkElement[0]).toBeInTheDocument();
 });
