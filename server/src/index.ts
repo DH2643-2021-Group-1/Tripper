0d8fe58c081eb1ebf8732ffd09d82342720e2bd2
Adam Jonsson <adamsmajl@gmail.com>
2021-10-16 23:05:03 +0200

    #15 devops: Add deployment using Docker & GCloud (#32)
    * feat: Add Docker and deploy method
    
    * fix: Add "on" to deploy action
    
    * fix: Use different gcloud action
    
    * fix: Remove GO tests
    
    * fix: Add firebase-key to deployment
    
    * fix: Replace existing content to firebase-key
    
    * debug: Test to log firebase key
    
    * fix: Correcly log firebase secrete
    
    * fix: Add firebase key correcly to github action
    
    * fix: Make the production build use correct port
    
    * doc: Add deployment instruction to readme

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(cors());
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
