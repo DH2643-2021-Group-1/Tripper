00edd2fdb4188374f3f794886d14ab4485dc1e6b
Adam Jonsson <adamsmajl@gmail.com>
2021-10-18 14:23:48 +0200

    feat: Add meta data for dev and production

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.tsx",
@@ -36,6 +37,11 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "public", "index.html"),
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: __dirname + '/public/PublicAssets', to: 'PublicAssets' },
+            ],
         }),
     ],
 };
