9ab1b3a53570946fe462448dc2348af1117a564f
annagustavsson <annalovisaellen@gmail.com>
2021-10-18 15:35:04 +0200

    feat: inital form rendering logic

--- client/src/components/pages/PostPage/PostPresenter.tsx
+++ client/src/components/pages/PostPage/PostPresenter.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PostView from "./PostView";
 
 const PostPresenter = () => {
-  return <PostView />;
+
+  const [formValue, setFormValue] = useState("")
+
+  const handleSubmit = () => {
+    console.log("handlesubmit place holder")
+  }
+
+  const onChange = (e: any) => {
+    const input = e.target.value;
+    setFormValue(input)
+  }
+
+
+  return <PostView onChange={onChange} onSubmit={handleSubmit} formValue={formValue} />;
 };
 
 export default PostPresenter;
--- client/src/components/pages/PostPage/PostView.tsx
+++ client/src/components/pages/PostPage/PostView.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./PostPage.scss";
+import Input from "../../input/input"
 
-const PostView = () => {
-  return <div>Post</div>;
+interface Props {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  onSubmit: Function,
+  formValue: string
+}
+
+const PostView = ({ onChange, onSubmit, formValue }: Props) => {
+  return <div style={{ border: "2px solid red" }}>
+    <Input name={"hello"} value={formValue} onChange={onChange} />
+  </div>;
 };
 
 export default PostView;
