85714934fc4487b625a7eb1396068e7a88627f7e
annagustavsson <annalovisaellen@gmail.com>
2021-10-20 15:00:34 +0200

    feat: call api hook for post + add title field

--- client/src/components/pages/PostPage/PostPresenter.tsx
+++ client/src/components/pages/PostPage/PostPresenter.tsx
@@ -1,21 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PostView from "./PostView";
+import useBlogPostApi from "../../../hooks/useBlogPostApi"
+import { BlogPost } from "../../../models/blog-post";
+
+// TODO: send img to firebase
+// TODO map image(s) to BLogPost interface before sending (hook needs changes)
+// ? hur kommer blogpostid till?
+
+// TODO possibility to populate fields with existing blogpost (for editing mode)
+// TODO: felhantering - "du måste ha en titel", etc
 
 const PostPresenter = () => {
 
-  const [formValue, setFormValue] = useState("")
+  const [handleGetAllBlogPosts, handleSetPost, handleGetBlogPostByUserId, handleGetBlogPostByPostId] = useBlogPostApi()
 
-  const handleSubmit = () => {
-    console.log("handlesubmit place holder")
+  const [blogPostContent, setblogPostContent] = useState("")
+  const [blogPostImages, setblogPostImages] = useState<Array<File>>([])
+  const [blogPostTitle, setblogPostTitle] = useState("")
+  const [loading, setIsLoading] = useState(false)
+
+
+  const handleSubmit = async () => {
+    // TODO user id should be included
+    setIsLoading(true)
+    await handleSetPost(blogPostTitle, blogPostContent)
+    setIsLoading(false)
   }
 
-  const onChange = (e: any) => {
-    const input = e.target.value;
-    setFormValue(input)
+  const handleTextChange = (e: any) => {
+    const blogpostText = e.target.value;
+    setblogPostContent(blogpostText)
   }
 
+  const handleChangeHeader = (e: any) => {
+    const blogpostTitle = e.target.value;
+    setblogPostTitle(blogpostTitle)
+  }
 
-  return <PostView onChange={onChange} onSubmit={handleSubmit} formValue={formValue} />;
+  const handleFileChange = (e: any) => {
+    const files: Array<File> = Array.from(e.target.files)
+    setblogPostImages(files)
+  }
+
+  return <PostView onHeadingChange={handleChangeHeader} onTextChange={handleTextChange} onImageChange={handleFileChange} onSubmit={handleSubmit} formValue={blogPostContent} formHeader={blogPostTitle} isLoading={loading} />;
 };
 
 export default PostPresenter;
--- client/src/components/pages/PostPage/PostView.tsx
+++ client/src/components/pages/PostPage/PostView.tsx
@@ -1,16 +1,52 @@
 import React from "react";
 import "./PostPage.scss";
 import Input from "../../input/input"
+import Button, { ButtonTypes } from "../../button/button"
+import LoadingIndicator from "../../loading-indicator/loading-indicator"
+
+
+
 
 interface Props {
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
-  onSubmit: Function,
-  formValue: string
+  onHeadingChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  onTextChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  onImageChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  onSubmit: () => void,
+  formValue: string,
+  formHeader: string,
+  isLoading: boolean
 }
 
-const PostView = ({ onChange, onSubmit, formValue }: Props) => {
+const PostView = ({ onHeadingChange, onTextChange, onImageChange, onSubmit, formValue, formHeader, isLoading }: Props) => {
   return <div style={{ border: "2px solid red" }}>
-    <Input name={"hello"} value={formValue} onChange={onChange} />
+    <Input name={"blogpost input form"} value={formValue} onChange={onTextChange} />
+    <Input name={"blogpost title input form"} value={formHeader} onChange={onHeadingChange} />
+    {isLoading && <LoadingIndicator />}
+    <Button
+      disabled={false}
+      type={ButtonTypes.primary}
+      onPress={onSubmit}
+    >
+      Post
+    </Button>
+
+    <div style={{ border: "2px solid blue" }}>
+      <div style={{ border: "1px solid green" }}>
+        <label htmlFor='single'>
+          Single img form
+        </label>
+        <input type='file' id='single' onChange={onImageChange} />
+      </div>
+    </div>
+    <div style={{ border: "2px solid blue" }}>
+      <div style={{ border: "1px solid green" }}>
+        <label htmlFor='multi'>
+          Multiple img form
+        </label>
+        <input type='file' id='multi' onChange={onImageChange} multiple />
+      </div>
+    </div>
+
   </div>;
 };
 
