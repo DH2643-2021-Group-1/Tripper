27cccd533aa6df413ac36b23baedb3f6ff28a349
Anna Gustavsson <58371851+annagustavsson@users.noreply.github.com>
2021-10-21 11:35:41 +0200

    Issue/31/fix blogpost page (#40)
    * feat: inital form rendering logic
    
    * feat: add spinner to LoadingIndicator component
    
    * feat: call api hook for post + add title field
    
    * chore: import file storage
    
    * feat: add image input component
    
    * style: justify content blogpost page
    
    * style: add padding
    
    * refactor: cleaning up func
    
    * feat: multiline input features in input form
    
    * fix: add prevent default
    
    * fix: some cleanup

---
+++ client/src/components/imageInput/ImageInput.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./imageInput.scss"
+import upload from "../../resources/images/upload.png";
+
+interface Props {
+    onImageChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+
+}
+
+const ImageInput = ({ onImageChange }: Props) => {
+    return (
+        <div>
+            <div >
+                <label htmlFor='single'>
+                    <img className="imageContainer" src={upload} alt="upload image" />
+                </label>
+                <input className="hide" type='file' id='single' onChange={onImageChange} />
+            </div>
+        </div>
+    )
+}
+
+export default ImageInput
--- server/src/db.ts
+++ server/src/db.ts
@@ -1,8 +1,12 @@
 import * as admin from 'firebase-admin';
+import { firestore } from 'firebase-admin';
 import * as serviceAccount from './firebase-key.json';
 
 const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
 
-export default app.firestore();
+
+export const db = app.firestore();
+export const storage = app.storage();
+
