7ba41396222d6334ff9ecdd418aa4be18d5f2b92
Adam Jonsson <adamsmajl@gmail.com>
2021-10-21 18:57:27 +0200

    feat: Add blog post editor to the blog post form

--- client/src/components/pages/PostPage/PostView.tsx
+++ client/src/components/pages/PostPage/PostView.tsx
@@ -4,9 +4,13 @@ import Input from "../../input/input"
 import ImageInput from "../../imageInput/ImageInput";
 import Button, { ButtonTypes } from "../../button/button"
 import LoadingIndicator from "../../loading-indicator/loading-indicator"
+import ContentWrapper from "../../content-wrapper/content-wrapper";
+import BlogPostContentPresenter from "../../blog-post-content/BlogPostContent/BlogPostContentPresenter";
+import { BlogPostContent } from "../../../models/blog-post-content/blog-post-content";
 
 
 interface Props {
+  onContentChange: (updatedContent: BlogPostContent) => void,
   onHeadingChange: (event: React.ChangeEvent<HTMLInputElement> | React.FormEvent<HTMLTextAreaElement>) => void,
   onTextChange: (event: React.ChangeEvent<HTMLInputElement> | React.FormEvent<HTMLTextAreaElement>) => void,
   onImageChange: (event: React.ChangeEvent<HTMLInputElement> | React.FormEvent<HTMLTextAreaElement>) => void,
@@ -15,12 +19,13 @@ interface Props {
   formHeader: string,
   isLoading: boolean,
   preview: string,
+  content: BlogPostContent
 }
 
-const PostView = ({ onHeadingChange, onTextChange, onImageChange, onSubmit, formValue, formHeader, isLoading, preview }: Props) => {
+const PostView = ({ onContentChange, onHeadingChange, onTextChange, onImageChange, onSubmit, formValue, formHeader, isLoading, preview, content }: Props) => {
   return <div className="flexbox">
-    <span>Create new blog post</span>
-    <div className="container">
+    <ContentWrapper>
+      <p>Create new blog post</p>
       <span>Title</span>
       <Input name={"blogpost input form"} multiLine={false} value={formValue} onChange={onTextChange} />
       <span>Description</span>
@@ -28,14 +33,24 @@ const PostView = ({ onHeadingChange, onTextChange, onImageChange, onSubmit, form
       {isLoading && <LoadingIndicator />}
       {preview && <img className="preview" src={preview} alt="preview image" />}
       <ImageInput onImageChange={onImageChange} />
+      <hr />
+      <br />
+    </ContentWrapper>
+    <BlogPostContentPresenter
+        content={content}
+        editMode={true}
+        onContentEdited={onContentChange}/>
+    <ContentWrapper>
+      <br />
+      <hr />
+      <br />
       <Button
-        disabled={false}
-        type={ButtonTypes.primary}
-        onPress={onSubmit}
-      >
-        Post
-    </Button>
-    </div>
+          disabled={false}
+          type={ButtonTypes.primary}
+          onPress={onSubmit}>
+            Post
+      </Button>
+    </ContentWrapper>
   </div>;
 };
 
