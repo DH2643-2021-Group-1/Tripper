d3ecff8c2cfa65615bab253834543931f0406cc9
Kristina Andersson <48219903+anderssonk@users.noreply.github.com>
2021-10-24 15:50:14 +0200

    feat: add user context (#85)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Menu from "./components/MenuPresenter/MenuPresenter";
@@ -8,46 +8,50 @@ import PostPresenter from "./components/pages/PostPage/PostPresenter";
 import EditPresenter from "./components/pages/EditPage/EditPresenter";
 import BlogPostPagePresenter from "./components/pages/BlogPostPage/BlogPostPagePresenter";
 
+import { AuthProvider } from "./contexts/AuthContext";
+
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={["/", "/home"]}>
-          <Menu />
-          <div className="pageContainer">
-            <WelcomePresenter />
-          </div>
-        </Route>
-        <Route path="/blog/:id">
-          <Menu />
-          <BlogPostPagePresenter />
-        </Route>
-        <Route exact path="/post">
-          <Menu />
-          <div className="pageContainer">
-            <PostPresenter />
-          </div>
-        </Route>
-        <Route exact path="/edit-post/:id">
-          <Menu />
-          <div className="pageContainer">
-            <PostPresenter />
-          </div>
-        </Route>
-        <Route exact path="/profile">
-          <Menu />
-          <div className="pageContainer">
-            <ProfilePresenter />
-          </div>
-        </Route>
-        <Route exact path="/edit">
-          <Menu />
-          <div className="pageContainer">
-            <EditPresenter />
-          </div>
-        </Route>
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+            <Menu />
+            <div className="pageContainer">
+              <WelcomePresenter />
+            </div>
+          </Route>
+          <Route path="/blog/:id">
+            <Menu />
+            <BlogPostPagePresenter />
+          </Route>
+          <Route exact path="/post">
+            <Menu />
+            <div className="pageContainer">
+              <PostPresenter />
+            </div>
+          </Route>
+          <Route exact path="/edit-post/:id">
+            <Menu />
+            <div className="pageContainer">
+              <PostPresenter />
+            </div>
+          </Route>
+          <Route exact path="/profile">
+            <Menu />
+            <div className="pageContainer">
+              <ProfilePresenter />
+            </div>
+          </Route>
+          <Route exact path="/edit">
+            <Menu />
+            <div className="pageContainer">
+              <EditPresenter />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
 
