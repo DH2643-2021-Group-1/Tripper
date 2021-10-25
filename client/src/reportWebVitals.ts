3aeb58d5237bed62a72bea23ca06c03f953313a5
Charlotte Andersson <charlotteandersson6@gmail.com>
2021-10-25 16:12:30 +0200

    test 3

--- client/src/reportWebVitals.ts
+++ client/src/reportWebVitals.ts
@@ -1,13 +1,13 @@
-import { ReportHandler } from 'web-vitals';
+import { ReportHandler } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getTTFB(onPerfEntry)
     });
   }
 };
